90e4e5e3b85d6230a631690c385278dde7f5e7d2
Josh Akeman <joshakeman@gmail.com>
2019-05-23 14:23:46 -0400

    three tests passing for user route

---
+++ users/users-model.spec.js
@@ -0,0 +1,47 @@
+const db = require('../database/dbConfig')
+const Users = require('./users-model')
+
+describe('insert()', () => {
+    afterEach(async () => {
+        await db('users').truncate()
+    })
+
+    it('should add the created user', async () => {
+        const { id } = await Users.add({
+            "username" : "user 3",
+            "password" : "password 3"
+        })
+
+        const users = await db('users').where({ id })
+
+        expect(users.length).toBeTruthy()
+    })
+
+    it('should add the correct user provided', async () => {
+        afterEach(async () => {
+            await db('users').truncate()
+        })
+
+        let user = await Users.add({ username: 'user 4', password: 'password 4' })
+        expect(user.username).toBe('user 4')
+  
+        user = await Users.add({ username: 'user 5', password: 'password 5' })
+        expect(user.username).toBe('user 5')
+  
+        const users = await db('users')
+  
+        expect(users).toHaveLength(2)
+      })
+
+      it('added user should have the required fields', async () => {
+        afterEach(async () => {
+            await db('users').truncate()
+        })
+
+        let user = await Users.add({ username: 'user', password: 'password' });
+
+        expect(user).toHaveProperty('username')
+        expect(user).toHaveProperty('password')
+
+    });   
+})
